2438d389747441bb990dff2f6943d1a2bb88fa06
Elian Fajardo Gutierrez <eliancfg.fajardo@gmail.com>
2026-04-23 18:10:07 -0600

    create base components

---
+++ examen1-progra4/src/Components/CarParts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CarParts = () => {
+    return (
+        <div>
+            car parts
+        </div>
+    )
+}
+
+export default CarParts
---
+++ examen1-progra4/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/">inicio</a></li>
+                    <li><a href="/about">repuestos</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
